b9d706d420a0b1ccf71115b7614c222f4b510720
Techharik <khariprasath30@gmail.com>
2024-04-09 12:39:01 +0530

    created a background color generator app

--- App.tsx
+++ App.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   StyleSheet,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  StatusBar
 } from 'react-native';
 
+
+const colors = ['#14213d']
 const App =()=>{
+  const {container,text} = styles;
+  const [randomColor, setRandomColor] = useState('')
+  const [colorCode ,setColorCode]=useState('')
+
+ 
+const rbg =()=>{
+   const rgbcolor = Math.floor(Math.random()*256)
+   return rgbcolor;
+}
+
+const changeBg =()=>{
+  const color = `rgb(${rbg()},${rbg()},${rbg()})`
+  setRandomColor(color);
+  setColorCode(color)
+}
+
   return (
   <>
-  <View style={styles.container}>
-     <Text>Hello EveryOne, I am Hari</Text>
-     <Text>Welcome to the Background color changer</Text>
+  <StatusBar backgroundColor={randomColor} />
+  <View style={[container , {backgroundColor:randomColor}]}>
+     <Text style={text}>{colorCode}</Text>
+    <TouchableOpacity onPress={changeBg}>
+     <Text style={text}>Click Here</Text>
+    </TouchableOpacity>
+     <Text style={{color:"#000"}}>Random Bg-color changer</Text>
   </View>
   </>
   )
@@ -22,9 +45,18 @@ export default App;
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor:'purple',
+    backgroundColor:'#14213d',
     flex:1,
     justifyContent:'center',
-    alignContent:'center'
+    alignItems:'center'
+  },
+  text:{
+     fontSize:20,
+     backgroundColor:'#fca311',
+     paddingVertical:10,
+     paddingHorizontal:20,
+     borderRadius:9,
+     color:'#000000',
+     marginBottom:12
   }
 })
